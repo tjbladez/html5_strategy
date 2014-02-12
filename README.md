ff5e2945c30a0f21f687717e364ace06725d2bbe
Nickolay Schwarz <tjbladez@gmail.com>
2014-02-12 17:56:11 -0600

    Adding basic express server and jade template

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ html5_strategy
 This is a project to capture development of the html5 strategy game. It will be accompanied with the collection of the pages on my thought process in development of game.
 
 For current state of the game please visit:
+
 For my notes, thoughts and development progress visit: http://tjbladez.github.io/html5_strategy/
---
+++ server.js
@@ -0,0 +1,21 @@
+(function() {
+  var express = require('express');
+  var app = express();
+  var port = process.env.PORT || 2828;
+  app.configure(function() {
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.methodOverride());
+    app.use(express.bodyParser());
+    app.use(express.static(__dirname));
+    return app.use(app.router);
+  });
+  app.get('/', function(req, res) {
+    return res.render('index.jade', {
+      locals: {
+        title: 'HTML5 Strategy Game'
+      }
+    });
+  });
+  app.listen(port);
+}).call(this);
