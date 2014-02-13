17356f4a10252bf07855cd5deefd44ace7d93ecf
Nickolay Schwarz <tjbladez@gmail.com>
2014-02-12 22:59:08 -0600

    messing with the package json

--- server.js
+++ server.js
@@ -2,13 +2,12 @@
   var express = require('express');
   var app  = express();
   var port = process.env.PORT || 2828;
-  process.env.PWD = process.cwd()
   app.configure(function() {
-    app.set('views', process.env.PWD + '/views');
+    app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
     app.use(express.methodOverride());
     app.use(express.bodyParser());
-    app.use(express.static(process.env.PWD + '/public'));
+    app.use(express.static(__dirname + '/public'));
     return app.use(app.router);
   });
   app.get('/', function(req, res) {
