5b099f321c8db03f78de748f46a654973dc9c03e
Nickolay Schwarz <tjbladez@gmail.com>
2014-02-12 22:42:23 -0600

    Adding styles back

--- server.js
+++ server.js
@@ -3,11 +3,11 @@
   var app = express();
   var port = process.env.PORT || 2828;
   app.configure(function() {
+    app.use(express.static(__dirname + '/public'));
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
     app.use(express.methodOverride());
     app.use(express.bodyParser());
-    app.use(express.static(__dirname+'/public'));
     return app.use(app.router);
   });
   app.get('/', function(req, res) {
